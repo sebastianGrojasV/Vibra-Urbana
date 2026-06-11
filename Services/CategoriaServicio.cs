6e3bab3781ff47cc161690d7dd0b2ab2684609d3
Alejandro150900 <luisaa00-@hotmail.com>
2026-06-11 17:24:33 -0600

    Creacion de Categorias con Validaciones

---
+++ Services/CategoriaServicio.cs
@@ -0,0 +1,42 @@
+﻿using VibraUrbana.Models;
+using VibraUrbana.Repositories;
+
+namespace VibraUrbana.Services
+{
+    public class CategoriaServicio : ICategoriaServicio
+    {
+        private readonly ICategoriaRepositorio _categoriaRepositorio;
+
+        public CategoriaServicio(ICategoriaRepositorio categoriaRepositorio)
+        {
+            _categoriaRepositorio = categoriaRepositorio;
+        }
+
+        public async Task<List<Categoria>> ObtenerCategoriasAsync()
+        {
+            return await _categoriaRepositorio.ObtenerCategoriasAsync();
+        }
+
+        public async Task<Categoria?> ObtenerPorIdAsync(int id)
+        {
+            return await _categoriaRepositorio.ObtenerPorIdAsync(id);
+        }
+
+        public async Task<bool> AgregarCategoriaAsync(Categoria categoria)
+        {
+
+            return await _categoriaRepositorio.AgregarCategoriaAsync(categoria);
+        }
+
+        public async Task<bool> ActualizarCategoriaAsync(Categoria categoria)
+        {
+            return await _categoriaRepositorio.ActualizarCategoriaAsync(categoria);
+        }
+
+        public async Task<bool> EliminarCategoriaAsync(int id)
+        {
+            return await _categoriaRepositorio.EliminarCategoriaAsync(id);
+        }
+
+    }
+}
---
+++ Services/ICategoriaServicio.cs
@@ -0,0 +1,13 @@
+﻿using VibraUrbana.Models;
+
+namespace VibraUrbana.Services
+{
+    public interface ICategoriaServicio
+    {
+        Task<List<Categoria>> ObtenerCategoriasAsync();
+        Task<Categoria?> ObtenerPorIdAsync(int id);
+        Task<bool> AgregarCategoriaAsync(Categoria categoria);
+        Task<bool> ActualizarCategoriaAsync(Categoria categoria);
+        Task<bool> EliminarCategoriaAsync(int id); // Eliminado lógico
+    }
+}
